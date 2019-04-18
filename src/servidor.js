19371e8bef87a2bea10144ce6f3e89f8f21a9a0b
Werner1201 <werner.romling@unigranrio.br>
2019-04-18 11:22:11 -0300

    primeira parte das aulas

--- src/servidor.js
+++ src/servidor.js
@@ -1,2 +1,17 @@
 //O que é uma porta ? é um processo no computador, ele precisa de uma porta para se comunicar com um app especifico
 const porta = 3003;
+const express = require("express");
+const app = express();
+/*
+essa function ira servir o conteudo para qualquer url que for chamada
+app.use((req, res, next) => {
+  res.send({ nome: "Notebook", preco: 123.45 }); //vai converter para json por causa do metodo
+});
+*/
+app.get("/produtos", (req, res, next) => {
+  res.send({ nome: "Notebook", preco: 123.45 }); //vai converter para json por causa do metodo
+});
+
+app.listen(porta, () => {
+  console.log(`Servidor está executando na porta ${porta}`);
+});
