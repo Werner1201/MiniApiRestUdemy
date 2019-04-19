8716d72299264918151d2ad62a403e5d93be5207
Werner1201 <werner.romling@unigranrio.br>
2019-04-19 09:35:15 -0300

    Terceira Aula, adcionando o Body Parser e Criando os medotos CRUD

--- src/servidor.js
+++ src/servidor.js
@@ -3,12 +3,15 @@ const porta = 3003;
 const express = require("express");
 const app = express();
 const bancoDeDados = require("./bancoDeDados");
+const bodyParser = require("body-parser");
 /*
 essa function ira servir o conteudo para qualquer url que for chamada
 app.use((req, res, next) => {
   res.send({ nome: "Notebook", preco: 123.45 }); //vai converter para json por causa do metodo
 });
 */
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/produtos", (req, res, next) => {
   res.send(bancoDeDados.getProdutos()); //vai converter para json por causa do metodo
 });
@@ -22,9 +25,24 @@ app.get("/produtos/:id", (req, res, next) => {
 //Agora para salvar um novo produto eh necessario utilizar o post
 app.post("/produtos", (req, res, next) => {
   const produto = bancoDeDados.salvarProduto({
-    nome: req.body.name,
+    nome: req.body.nome,
     preco: req.body.preco
   });
+  res.send(produto); //JSON
+});
+
+//Para alterar o que esta no banco
+app.put("/produtos/:id", (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    id: req.params.id,
+    nome: req.body.nome,
+    preco: req.body.preco
+  });
+  res.send(produto); //JSON
+});
+
+app.delete("/produtos/:id", (req, res, next) => {
+  const produto = bancoDeDados.excluirProduto(req.params.id);
   res.send(produto); //JSON
 });
 
