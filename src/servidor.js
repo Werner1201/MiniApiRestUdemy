a4665279e9b944120744c4396823de0e7e3d78cd
Werner1201 <werner.romling@unigranrio.br>
2019-04-19 09:14:50 -0300

    Segunda Aula COM ERROS

--- src/servidor.js
+++ src/servidor.js
@@ -2,6 +2,7 @@
 const porta = 3003;
 const express = require("express");
 const app = express();
+const bancoDeDados = require("./bancoDeDados");
 /*
 essa function ira servir o conteudo para qualquer url que for chamada
 app.use((req, res, next) => {
@@ -9,7 +10,22 @@ app.use((req, res, next) => {
 });
 */
 app.get("/produtos", (req, res, next) => {
-  res.send({ nome: "Notebook", preco: 123.45 }); //vai converter para json por causa do metodo
+  res.send(bancoDeDados.getProdutos()); //vai converter para json por causa do metodo
+});
+
+//:id eh acesso a um parametro na url
+//esse id eh pego do request req por que esta na requisicao
+app.get("/produtos/:id", (req, res, next) => {
+  res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+//Agora para salvar um novo produto eh necessario utilizar o post
+app.post("/produtos", (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.name,
+    preco: req.body.preco
+  });
+  res.send(produto); //JSON
 });
 
 app.listen(porta, () => {
